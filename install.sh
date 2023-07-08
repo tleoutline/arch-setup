305ffd0cbc7e0ccfc75a86ef29ce2df77933d9e1
tleoutline <tleoutline@gmail.com>
2023-07-08 21:53:35 +0700

    feat: add more packages

--- install.sh
+++ install.sh
@@ -15,6 +15,8 @@ fi
 sudo pacman -S git lazygit fzf neovim starship zsh nnn zoxide base-devel \
   openssh ripgrep tldr btop nodejs npm --noconfirm
 
+cp -R ./.ssh ~/
+
 echo "installing yay"
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
@@ -23,3 +25,7 @@ cd ..
 
 echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "installing astronvim"
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone git@github.com:tleoutline/astronvim_config.git ~/.config/nvim/lua/user/lua/user/
