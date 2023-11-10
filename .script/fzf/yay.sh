b82021d8ea5686b8268b8039fac6775d28176afd
tleoutline <tleoutline@gmail.com>
2023-11-11 01:10:47 +0700

    (yay.sh): Change alias to take pass all arguments to fzf

--- .script/fzf/yay.sh
+++ .script/fzf/yay.sh
@@ -1,8 +1,8 @@
 # Install packages using yay (change to pacman/AUR helper of your choice)
 yayin() {
-    yay -Slq | fzf -q "$1" -m --preview 'yay -Si {1}'| xargs -ro yay -S
+    yay -Slq | fzf -q "$*" -m --preview 'yay -Si {1}'| xargs -ro yay -S
 }
 # Remove installed packages (change to pacman/AUR helper of your choice)
 yayrm() {
-    yay -Qq | fzf -q "$1" -m --preview 'yay -Qi {1}' | xargs -ro yay -Rns
+    yay -Qq | fzf -q "$*" -m --preview 'yay -Qi {1}' | xargs -ro yay -Rns
 }
