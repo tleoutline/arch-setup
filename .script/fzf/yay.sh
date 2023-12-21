cdc3969a12fce2d941c58cff78fd22db58a66f7a
tleoutline <tleoutline@gmail.com>
2023-12-22 01:51:35 +0700

    (fzf-yay): change preview window size

--- .script/fzf/yay.sh
+++ .script/fzf/yay.sh
@@ -1,8 +1,8 @@
 # Install packages using yay (change to pacman/AUR helper of your choice)
 yayin() {
-    yay -Slq | fzf -q "$1" -m --preview 'yay -Si {1}' --preview-window 'right,60%' | xargs -ro yay -S
+    yay -Slq | fzf -q "$1" -m --preview 'yay -Si {1}' --preview-window 'right,70%' | xargs -ro yay -S
 }
 # Remove installed packages (change to pacman/AUR helper of your choice)
 yayrm() {
-    yay -Qq | fzf -q "$1" -m --preview 'yay -Qi {1}' --preview-window 'right,60%' | xargs -ro yay -Rns
+    yay -Qq | fzf -q "$1" -m --preview 'yay -Qi {1}' --preview-window 'right,70%' | xargs -ro yay -Rns
 }
